2bfeba95f1d279375459bcd9840b0add5efb712d
Tomas Babej <tomas@tbabej.com>
2018-05-20 19:08:53 -0400

    rename experiments

--- exp_1_histogram.py
+++ exp_1_histogram.py
@@ -12,14 +12,6 @@ def sample_and_histogram(sampler, model, temperature):
     seaborn.distplot(results.raw_data, rug=False, kde=False, bins=30)
     plt.show()
 
-
-def determine_gibbs_params(n_variables):
-    burn_in = int(0.7 * n_variables ** 2)
-    thinning = int(0.1 * n_variables) + 3
-
-    return burn_in, thinning
-
-
 def main():
     model_width = 4
     model_height = 4
@@ -32,8 +24,7 @@ def main():
     sample_and_histogram(sampler, model, temperature=1)
 
     # Then with gibbss sampleler
-    gibbs_params = determine_gibbs_params(model_width * model_height)
-    sampler = GibbsSampler(*gibbs_params)
+    sampler = GibbsSampler(n_variables=model_height * model_width)
     sample_and_histogram(sampler, model, temperature=1)
 
     # Compute partition function of this model
