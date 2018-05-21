4cc01a4a876ed5b853c053eff188ce144cf9ac84
Tomas Babej <tomas@tbabej.com>
2018-05-20 20:34:52 -0400

    variation in sizes

--- exp_2_kl.py
+++ exp_2_kl.py
@@ -10,40 +10,37 @@ from util import save_experiment
 
 
 def main():
-    #for width in range(3,6):
-    #    for height in range(width, 6):
-    #        model_width = width
-    #        model_height = height
-    model_width = 4
-    model_height = 4
-    decimals = 2
-    temperature = 1
+    for width in range(3,6):
+        for height in range(width, 6):
+            model_width = width
+            model_height = height
+            decimals = 2
+            temperature = 1
 
-    # Create the model
-    builder = RandomBuilder(model_width, model_height)
-    model = builder.generate(decimals=decimals)
+            # Create the model
+            builder = RandomBuilder(model_width, model_height)
+            model = builder.generate(decimals=decimals)
 
-    # Compute partition function of this model
-    bruteforcer = BruteforceSampler()
-    Z = bruteforcer.partition_function(model, temperature)
-    print("Partition function: {}".format(Z))
+            # Compute partition function of this model
+            bruteforcer = BruteforceSampler()
+            Z = bruteforcer.partition_function(model, temperature)
 
-    # First produce a graph with D-Wave
-    sampler = DWaveSampler()
-    results = sampler.sample(model, 1000, temperature)
+            # First produce a graph with D-Wave
+            sampler = DWaveSampler()
+            results = sampler.sample(model, 10000, temperature)
 
-    kl = results.KL_divergence(Z, temperature)
-    print("KL divergence: {}".format(kl))
+            kl = results.KL_divergence(Z, temperature)
+            print("KL divergence: {}".format(kl))
 
-    save_experiment("kl", {
-        'width': model_width,
-        'height': model_height,
-        'model_decimals': decimals,
-        'temperature': temperature,
-        'dwave_samples': results.raw_data,
-        'z': Z,
-        'kl': kl
-    })
+            save_experiment("kl", {
+                'width': model_width,
+                'height': model_height,
+                'model_decimals': decimals,
+                'temperature': temperature,
+                'dwave_samples': results.raw_data,
+                'z': Z,
+                'kl': kl
+            })
 
 
 if __name__ == '__main__':
